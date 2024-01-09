c10f503d8d26ab181de386f0ca630106359751c7
Joey Dreijer <joey.dreijer@hema.nl>
2024-01-09 16:27:18 +0100

    Added link to github source

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -6,8 +6,10 @@
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>  
       </div>
-      <div class="text-3xl font-bold uppercase dark:text-slate-200">
-        Decon
+      <div class="text-3xl font-bold uppercase dark:text-slate-200 flex">
+        <div>
+          <a href="https://github.com/d3vzer0/decon-content">Decon</a>
+        </div>
       </div>
     </div>
     <div class="text-3xl font-bold uppercase dark:text-slate-200">
