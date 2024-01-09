445d30ffc594fc57fff10d0a902cc9bd310bea0e
Joey Dreijer <joey.dreijer@hema.nl>
2024-01-09 02:17:31 +0100

    Added responsive padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 import AppHeader from '../components/AppHeader.vue';
 <template>
-  <div class="max-w-full p-10">
+  <div class="max-w-full md:p-10 sm:p-5 p-2">
     <AppHeader />
     <slot />
   </div>
